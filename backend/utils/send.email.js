ef07c099b2bbed57a8d9385146002f4e74ce3455
Dharmendra Behera <dharmendrabehera680@gmail.com>
2026-09-20 19:57:21 +0530

    Update send.email.js

--- backend/utils/send.email.js
+++ backend/utils/send.email.js
@@ -5,7 +5,34 @@ const transporter = nodemailer.createTransport({
 
   auth: {
     user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,aimport nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = async ({
+  to,
+  subject,
+  text,
+  html,
+}) => {
+  await transporter.sendMail({
+    from: `"Water Delivery" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text,
+    html,
+  });
+};
+
+export default sendEmail;
   },
 });
 
